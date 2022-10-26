ac0868f3a996e7ce548ecec7efd4b9c61d526b40
curvasdoastro <112182493+curvasdoastro@users.noreply.github.com>
2022-10-26 08:17:09 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,8 +1,8 @@
+let tabuada = 1;
+let nome = "beatriz pongilio";
 
-let tabuada = 15;
-let nome = "gab"
-function escreva () {
-    document.write("tabuada do " + tabuada + ">br<")
+function escreva(){
+    document.write("tabuada do " + tabuada + "<br>")
     document.write(tabuada + " x 1 = "+(tabuada*1)+"<br>");
     document.write(tabuada + " x 2 = "+(tabuada*2)+"<br>");
     document.write(tabuada + " x 3 = "+(tabuada*3)+"<br>");
@@ -12,52 +12,72 @@ function escreva () {
     document.write(tabuada + " x 7 = "+(tabuada*7)+"<br>");
     document.write(tabuada + " x 8 = "+(tabuada*8)+"<br>");
     document.write(tabuada + " x 9 = "+(tabuada*9)+"<br>");
-    document.write(tabuada + " x 10 = "+(tabuada*10)+"<br>");
-document.write("feito por " + nome);
+    document.write(tabuada + " x 10 = "+(tabuada*10)+"<br><br>");
+    document.write("feito por " + nome);
 }
 
 function minhaTabuada(){
     for(let i = 1; i <= 10; i++){
-        document.write("Tabuada do " + i + "<br>");
+        document.write("Tabuada do "  + i + "<br>");
         for(let j = 1; j <= 10; j++){
             document.write(i + " x " + j + " = "+(j*i)+"<br>");
-{
-    document.write("<br>");
-
-       }
+        }
+      document.write("<br>");
     }
-unction quadrado (){
-  for(let i = 2; i <= 20; i++){
-    document.write("O quadrado de " + 1 + " é " + (i*i) + "<br>");
-  }
 }
 
+function quadrado(){
+    for(let i = 1; i <= 499; i++){
+    document.write("O quadrado de " + i + " é " + (i*i)+ "<br>");
+    }
+}
+function moeda(atual){
+    return atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
 
-function calcula() {
-  let c = document.getElementById("valor").value;
-  let j = document.getElementById("juros").value;
-  let r = c * (1 + (j/100));
-  document.write("Resultado: " + r);
 }
 
-
-
-document.write(Resultado= R$ + r)
+function calcula(){
+    let c = document.getElementById("valor").value;
+    let j = document.getElementById("juros").value;
+    if(!Number(c)){
+alert("o valor do capital deve ser numerico")
+document.getElementById("valor").value ="";
+document.getElementById("valor").focus();
+        return
+    }
+    if(!Number(j)){
+        alert("o valor do juros deve ser numerico")
+        document.getElementById("juros").value = "";
+        document.getElementById("juros").focus();
+                return
+            }
+    let r = c *(1 + (j/100));
+    document.write("resultado: " + moeda(r));
 }
 
 let op = "";
 function operacao(ope){
   op = ope;
 }
-function calcule (){
-  let v1 = document.getElementById("v1").value;
-  let v2 = document.getElementById("v2").value;
-  let r = 0;
-  // Isso deve ser duplicado a cada botão.
-  if(op == "+"){
-    r = Number(v1) + Number(v2);
-  }
+function calcule(){
+    let v1 = document.getElementById("v1").value;
+    let v2 = document.getElementById("v2").value;
+    let r =0;
 
-  document.getElementById("resultado").innerHTML = r;
-  
+    if(op == "+"){
+        r = Number(v1) + Number(v2);
+    }
+    if(op == "-"){
+        r = Number(v1) - Number(v2);
+    }
+    if(op == "*"){
+        r = Number(v1) * Number(v2);
+    }
+    if(op == "/"){
+        r = Number(v1) / Number(v2);
+    }
+
+
+    document.getElementById("resultado").innerHTML = r;
+
 }
